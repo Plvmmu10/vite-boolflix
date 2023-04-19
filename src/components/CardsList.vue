61fad45de422ab0fa8b0150b9308ad4cefda87e2
peppeluc22 <gluca0260@gmail.com>
2023-04-19 18:32:40 +0200

    splitted content in two rows

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,8 +1,18 @@
 <template>
-    <div class="row">
-        <CardNetflix v-for="card in cards.cardsList" :key="card.id" :title="card.title" :originalTitle="card.original_title"
-            :language="card.original_language" :vote="card.vote_average" :name="card.name"
-            :originalName="card.original_name" />
+    <div id="filmContainer">
+        <h1>Film consigliati</h1>
+        <div class="row">
+            <CardNetflix v-for="card in cards.cardsMovie" :key="card.id" :title="card.title"
+                :originalTitle="card.original_title" :language="card.original_language" :vote="card.vote_average" />
+        </div>
+    </div>
+
+    <div id="seriesContainer">
+        <h1>Serie TV consigliate</h1>
+        <div class="row" id="seriesRow">
+            <CardNetflix v-for="card in cards.cardsSeries" :key="card.id" :name="card.name"
+                :originalName="card.original_name" :language="card.original_language" :vote="card.vote_average" />
+        </div>
     </div>
 </template>
 
@@ -18,11 +28,26 @@ export default {
         return {
             cards
         }
-    },
-    methods: {
-
     }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+h1 {
+    color: white;
+    width: 100%;
+}
+
+.row {
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+}
+
+#filmContainer,
+#seriesContainer {
+    margin-bottom: 4rem;
+}
+</style>
