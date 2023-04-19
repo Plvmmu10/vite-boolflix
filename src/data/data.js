f561808311590f39c930dbf0d5e04d75b6be7cdb
peppeluc22 <gluca0260@gmail.com>
2023-04-19 18:34:27 +0200

    added seriesSearch

--- src/data/data.js
+++ src/data/data.js
@@ -1,15 +1,24 @@
 import { reactive } from 'vue';
 
 export const cards = reactive({
-    cardsList: [],
+    cardsMovie: [],
+    cardsSeries: [],
     basepath: ' https://api.themoviedb.org/3/',
     authkey: '?api_key=0360a04bf530bcd6435644bf9a3e0d7b',
     endpoint: '',
     moviepoint: 'search/movie',
+    seriespoint: 'search/tv',
     filmSearch: {
         query: '',
         title: '',
         original_title: '',
+        original_language: '',
+        vote_average: ''
+    },
+    seriesSearch: {
+        query: '',
+        name: '',
+        original_name: '',
         original_language: '',
         vote_average: ''
     }
