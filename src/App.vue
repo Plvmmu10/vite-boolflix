636160ca786fc96c19e13c04d3a8beff4bbdc0c2
peppeluc22 <gluca0260@gmail.com>
2023-04-19 14:00:24 +0200

    added NavbarNetflix component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<template></template>
+<template>
+  <header>
+    <NavbarNetflix />
+  </header>
+</template>
 
 <script>
+import NavbarNetflix from './components/NavbarNetflix.vue';
 export default {
   name: 'App',
-
+  components: {
+    NavbarNetflix
+  }
 }
 </script>
 
