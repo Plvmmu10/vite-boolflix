183d5f4cc6afddcb6ff1ed25b22bb3190ecbce72
peppeluc22 <gluca0260@gmail.com>
2023-04-19 16:13:04 +0200

    created cardList

---
+++ src/components/CardsList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <CardNetflix v-for="card in cards.cardsList" :key="card.id" :title="card.title" :originalTitle="card.original_title"
+            :language="card.original_language" :vote="card.vote_average" />
+    </div>
+</template>
+
+<script>
+import { cards } from '../data/data.js'
+import CardNetflix from './CardNetflix.vue';
+export default {
+    name: 'CardsList',
+    components: {
+        CardNetflix
+    },
+    data() {
+        return {
+            cards
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
