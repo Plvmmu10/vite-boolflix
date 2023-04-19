d152dd31423afb36b44c7999864353bfcba92106
peppeluc22 <gluca0260@gmail.com>
2023-04-19 16:12:51 +0200

    created card

---
+++ src/components/CardNetflix.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="col-3">
+        <div class="card">
+            <div class="card-image"></div>
+            <div class="card-body">
+                <h3>{{ title }}</h3>
+                <h6>{{ originalTitle }}</h6>
+                <p>{{ language }}</p>
+                <p>{{ vote }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CardNetflix',
+    props: ['title', 'originalTitle', 'language', 'vote', 'name', 'originalName'],
+
+}
+</script>
+
+<style lang="scss" scoped></style>
