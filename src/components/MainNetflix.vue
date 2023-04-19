ea77b883343582a4960bc212571d1d4d22ebdb12
peppeluc22 <gluca0260@gmail.com>
2023-04-19 16:13:23 +0200

    created Main and imported cardList

---
+++ src/components/MainNetflix.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <CardsList />
+    </div>
+</template>
+
+<script>
+import CardsList from './CardsList.vue';
+export default {
+    name: 'MainNetflix',
+    components: {
+        CardsList
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    margin-top: 100px;
+}
+</style>
