6f36fbeb3725831e8231119b0a099c812593b0be
peppeluc22 <gluca0260@gmail.com>
2023-04-20 17:33:04 +0200

    added toprated and popular point

--- src/data/data.js
+++ src/data/data.js
@@ -3,11 +3,15 @@ import { reactive } from 'vue';
 export const cards = reactive({
     cardsMovie: [],
     cardsSeries: [],
+    popularMovie: [],
+    topratedMovie: [],
     basepath: ' https://api.themoviedb.org/3/',
     authkey: '?api_key=0360a04bf530bcd6435644bf9a3e0d7b',
     endpoint: '',
     moviepoint: 'search/movie',
     seriespoint: 'search/tv',
+    topratedpoint: 'movie/top_rated',
+    popularpoint: 'movie/popular',
     imagePath: 'https://image.tmdb.org/t/p/original',
     filmSearch: {
         query: ''
