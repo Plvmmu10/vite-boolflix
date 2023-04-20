3acdea5081228a5338d79e3480b8b608ca2f1c0e
peppeluc22 <gluca0260@gmail.com>
2023-04-20 12:52:08 +0200

    changed from input to keyup

--- src/components/NavbarNetflix.vue
+++ src/components/NavbarNetflix.vue
@@ -2,12 +2,15 @@
     <header>
         <div id="myNavbar" class="d-flex justify-content-between align-items-center">
             <div class="title-container">
-                <h1 class="fw-bold m-0">Netflix</h1>
+                <h1 class="fw-bold m-0 text-uppercase">Boolflix</h1>
             </div>
 
             <div class="input-container">
                 <input type="text" name="searchBar" id="searchBar" placeholder="Cerca il tuo film" class="rounded-2 p-1"
-                    @input="goSearch" v-model.trim="cards.filmSearch.query">
+                    @keyup.enter="goSearch" v-model.trim="cards.filmSearch.query">
+
+                <button class="btn" @click="goSearch">Cerca</button>
+
             </div>
         </div>
     </header>
@@ -47,6 +50,26 @@ header {
         margin: 0 auto;
         padding: 1rem;
         color: $secondary-color;
+
+        .title-container {
+            cursor: pointer;
+        }
+
+        #searchBar {
+            background-color: transparent;
+            color: white;
+            border: 0;
+        }
+
+        button {
+            color: white;
+
+            &:hover {
+                color: $secondary-color;
+            }
+        }
+
+
     }
 }
 </style>
