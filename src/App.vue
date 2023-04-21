d2d60124da5d81d21240e56095bd9e0bad0a8b13
peppeluc22 <gluca0260@gmail.com>
2023-04-21 12:50:03 +0200

    added flags and comments

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,8 @@ export default {
   },
   methods: {
     newSearch() {
+
+      // All urls
       const filmUrl = cards.basepath + cards.moviepoint + cards.authkey + cards.endpoint;
       const seriesUrl = cards.basepath + cards.seriespoint + cards.authkey + cards.endpoint;
       const popularUrl = cards.basepath + cards.popularpoint + cards.authkey + cards.endpoint;
@@ -50,20 +52,50 @@ export default {
         searched.params = params;
       }
 
+
+      // All API Calls
       axios.get(filmUrl, searched).then((res) => {
-        cards.cardsMovie = res.data.results;
+        cards.cardsMovie = res.data.results.map((item) => {
+          const lang = { en: 'gb', ja: 'jp', cs: 'cz', zh: 'cn' };
+          return {
+            ...item,
+            original_language:
+              lang[item.original_language] || item.original_language
+          }
+        })
       })
 
       axios.get(popularUrl, searched).then((res) => {
-        cards.popularMovie = res.data.results;
+        cards.popularMovie = res.data.results.map((item) => {
+          const lang = { en: 'gb', ja: 'jp', cs: 'cz', zh: 'cn' };
+          return {
+            ...item,
+            original_language:
+              lang[item.original_language] || item.original_language
+          }
+        })
       })
 
       axios.get(ratedUrl, searched).then((res) => {
-        cards.topratedMovie = res.data.results;
+        cards.topratedMovie = res.data.results.map((item) => {
+          const lang = { en: 'gb', ja: 'jp', cs: 'cz', zh: 'cn' };
+          return {
+            ...item,
+            original_language:
+              lang[item.original_language] || item.original_language
+          }
+        })
       })
 
       axios.get(seriesUrl, searched).then((res) => {
-        cards.cardsSeries = res.data.results;
+        cards.cardsSeries = res.data.results.map((item) => {
+          const lang = { en: 'gb', ja: 'jp', cs: 'cz', zh: 'cn' };
+          return {
+            ...item,
+            original_language:
+              lang[item.original_language] || item.original_language
+          }
+        })
       })
 
     }
