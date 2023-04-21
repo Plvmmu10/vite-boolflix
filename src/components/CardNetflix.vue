6b2a62da1505e6acd4845648d96ecad19d3d7680
peppeluc22 <gluca0260@gmail.com>
2023-04-21 12:51:11 +0200

    added comments and language

--- src/components/CardNetflix.vue
+++ src/components/CardNetflix.vue
@@ -1,18 +1,31 @@
 <template>
     <div class="col-sm-6 col-md-5 col-lg-3 p-2">
         <div class="card border-0 h-100 w-100">
+
+            <!-- Card image -->
             <div class="card-image" :class="show ? 'active' : 'deactive'" @mouseenter="showInfo" @mouseleave="showInfo">
                 <img :src="imageUrl" :alt="card.title" class="img-fluid w-100 h-100">
             </div>
+
+            <!-- Card body with film/series title/name, overview, language and rating -->
             <div class="card-body" :class="show ? 'deactive' : 'active'" @mouseenter="showInfo" @mouseleave="showInfo">
+                <!-- Film/series tv name -->
                 <h3>{{ card.title }}</h3>
                 <h3>{{ card.name }}</h3>
 
+
+                <!-- Film/series tv overview -->
                 <div class="movie-info my-4">
                     <p>{{ card.overview }}</p>
                 </div>
 
+                <!-- Div with language and rating -->
                 <div class="rating mt-5">
+                    <p>
+                        Lingua:
+                        <span class="px-2" :class="'fi fi-' + card.original_language + ' fis'"></span>
+                    </p>
+
                     <i class="fa-star" v-for="star in 5" :class="(star <= stars ? 'fa-solid' : 'fa-regular')"></i>
 
                 </div>
@@ -24,6 +37,7 @@
 
 <script>
 import { cards } from '../data/data.js';
+import 'flag-icons/css/flag-icons.min.css';
 export default {
     name: 'CardNetflix',
     props: ['card'],
@@ -79,7 +93,7 @@ export default {
     transition: opacity .7s;
 
     .movie-info {
-        height: 150px;
+        height: 125px;
         overflow: auto;
     }
 
